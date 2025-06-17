b6a735c3094b568b7467626dd2aead3b0251c68a
gusrbs-0404 <gusrbs7534@gmail.com>
2025-06-18 00:33:35 +0900

    add Level6 Class4 중앙 이동 알고리즘 내 코드 추가

--- src/Level6/Class4.java
+++ src/Level6/Class4.java
@@ -11,6 +11,15 @@ public class Class4 {
         int n = Integer.parseInt(br.readLine());
         br.close();
 
+//        int A = 1;
+//        for (int i = 0; i < n; i++) {
+//            A *= 2;      // 2를 n번 곱함
+//        }
+//
+//        int B = A + 1;
+//        System.out.println(B * B);
+        // -------------------------------------
+
         int a = (int) (Math.pow(2, n) + 1); // 2 - 3 - 5 - 9
         int b = (int) (Math.pow(a, 2));
 
